6d28f465d14a038263b129aea6203ee8034807c1
miran00 <58536501+miran00@users.noreply.github.com>
2020-05-23 17:37:33 +0900

    Create product.java

---
+++ 2/src/product.java
@@ -0,0 +1,26 @@
+public class Product implements Comparable<Product>{
+	
+	private String productName;
+	private int productNum;
+	public void Person(String productName, int productNum) {
+		this.productName = productName;
+		this.productNum = productNum;
+	}
+	
+	public String getProductName() {
+		return productName;
+	}
+	
+	public int getProductNum() {
+		return productNum;
+	}
+	@Override
+	public int compareTo(Product compareProduct) {
+		// TODO Auto-generated method stub
+		return this.productNum - compareProduct.productNum;
+	}
+
+	public String toString() {
+		return "productName" + productName + "prodcutNum: " + productNum;
+	}
+}
